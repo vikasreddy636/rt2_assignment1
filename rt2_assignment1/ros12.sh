fc2b8ef1c4c61bbfc58408fce87ecb74e8136191
vikasreddy636 <vikasreddy636@rediffmail.com>
2022-01-03 20:45:40 +0000

    bridge done

---
+++ rt2_assignment1/ros12.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /root/my_ros/devel/setup.bash
+source /root/my_ros2/install/setup.bash
---
+++ rt2_assignment1/ros2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /opt/ros/foxy/setup.bash
+source /root/my_ros2/install/local_setup.bash
