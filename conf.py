ab4509fa1a3f2f0b163aa4dd367239a1b14e1da3
vikasreddy636 <vikasreddy636@rediffmail.com>
2022-02-09 17:53:15 +0000

    updated sphinx

--- conf.py
+++ conf.py
@@ -10,12 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import subprocess
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
-
+subprocess.call('doxygen Doxyfile.in', shell=True)
 # -- Project information -----------------------------------------------------
+
+
 
 project = 'rt2_assignment2'
 copyright = '2022, Ginne Vikas Reddy'
@@ -31,6 +34,18 @@ release = '09/02/2022'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    "sphinx.ext.napoleon",
+    'sphinx.ext.inheritance_diagram',
+    'breathe'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +62,27 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+highlight_language = 'c++'
+source_suffix = '.rst'
+master_doc = 'index'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
+# -- Options for intersphinx extension ---------------------------------------
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/': None}
+# -- Options for todo extension ----------------------------------------------
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
+# -- Options for breathe
+breathe_projects = {
+    "rt2_assignment2": "_build/xml/"
+}
+breathe_default_project = "rt2_assignment2"
+breathe_default_members = ('members', 'undoc-members')
--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -1,9 +1,29 @@
 #! /usr/bin/env python -tt
+
+"""
+.. module:: user_interface
+:platform: Unix
+:synopsis: Python module for the user Interface
+.. moduleauthor:: Ginne Vikas Reddy vikasreddy636@rediffmail.com
+This node implements an user interface
+Service:
+/user_interface
+"""
 import rospy
 import time
 from rt2_assignment1.srv import Command
 
 def main():
+    """
+    This function initializes the ROS node and waits for the
+    user to
+    insert *start* or *stop* to control the robot, by relying
+    on the
+    `rospy <http://wiki.ros.org/rospy/>`_ module.
+    The user message is passed to the service
+    ``user_interface``,
+    advertised by :mod:`go_to_point`.
+    """
     rospy.init_node('user_interface')
     ui_client = rospy.ServiceProxy('/user_interface', Command)
     time.sleep(10)
